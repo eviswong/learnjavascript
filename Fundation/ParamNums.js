5c39b61f0e314e1685dbab322612a5548d91359b
RAZER\sye.wang <sye.wang@razer.com>
2022-10-25 17:44:42 +0800

    Get number of the Parameter

--- Fundation/ParamNums.js
+++ Fundation/ParamNums.js
@@ -4,12 +4,6 @@ function foo(a, b, c) {
     return arguments.length;
 }
 
-function get_arguments() {
-    var argList = [];
-    for (var i = 0; i < arguments.length; i ++) {
-        argList.push(arguments[i]);
-    }
-}
+
 
 console.log(foo(1,2));
-console.log(get_arguments(1,2,3,4,5));
