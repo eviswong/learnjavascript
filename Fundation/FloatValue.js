c807874600b617989fbbf733af07041cd3fbe2b2
DESKTOP-BF7GKNI\wsy <1254547339@qq.com>
2022-11-05 12:34:17 +0800

    Add number calculation examples

---
+++ Fundation/FloatValue.js
@@ -0,0 +1,9 @@
+let pi = 314e-2;
+let e = 2.718;
+
+console.log("Pi = " + pi);
+
+let zeroPointThree = 0.1 + 0.1 + 0.1;
+console.log("zeroPointThree:" + zeroPointThree);
+
+console.log("zeroPointThree == 0.3 ?" + zeroPointThree == 0.3);
---
+++ Fundation/Integer.js
@@ -0,0 +1,12 @@
+var nosense = 0 / 0;
+
+console.log(nosense);
+
+var negInf = -1 / 0;
+console.log(negInf);
+
+var posInf = 1 / 0;
+console.log(posInf);
+
+var absZero = -1 / Infinity;
+console.log(absZero);
